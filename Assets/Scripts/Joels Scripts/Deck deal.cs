ada330c73cf84403be4c0ccfbf854b67ce026148
J0EL7031 <144268814+J0EL7031@users.noreply.github.com>
2024-03-12 10:35:38 -0700

    deck start

---
+++ Assets/Scripts/Joels Scripts/Deck deal.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deckdeal : MonoBehaviour
+{
+    public int red;
+    public int blue;
+    public int green;
+    public int grey;
+    public int black;
+    public int deck;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        red = 6;
+        blue = 6;
+        green = 6;
+        grey = 10;
+        black = 4;
+
+        deck = 32;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        deck = red + blue + green + grey + black;
+    }
+}
